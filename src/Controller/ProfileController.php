fc0b6e8f021013987548893c9e58e37ef51f7828
Hugo <hugo.leveque@efrei.net>
2025-12-09 19:46:20 +0100

    Modification de la page profile pour permettre les modifications et meilleur UI

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_home');
+        }
+
+        $user = $this->getUser();
+
+        if ($request->isMethod('POST')) {
+            if ($this->isCsrfTokenValid('update_profile', $request->request->get('_token'))) {
+                
+                $user->setUsername($request->request->get('username'));
+                $user->setEmail($request->request->get('email'));
+                
+                $entityManager->flush();
+            }
+
+            return $this->redirectToRoute('app_profile');
         }
 
         return $this->render('profile/index.html.twig', [
