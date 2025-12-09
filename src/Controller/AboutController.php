4ca3ee0a57c8a84f5af642a8d07182f1d5169383
Hugo <hugo.leveque@efrei.net>
2025-12-09 16:33:43 +0100

    Ajout page qui somme nous ?

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ProfileController extends AbstractController
+final class AboutController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+    #[Route('/about', name: 'app_about')]
     public function index(): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
         ]);
     }
 }
